f02b5e48a5182660540c2104aae4dd002e1e334c
Adam Bradbury <abradbury@users.noreply.github.com>
2019-02-10 11:13:10 +0000

    Refactor to handle difference output sources

--- main.py
+++ main.py
@@ -11,20 +11,115 @@ from machine import RTC
 from L76GNSS import L76GNSS
 from pytrack import Pytrack
 
-time.sleep(2)
-gc.enable()
+# Unfortunately, the abstract base class (abc) module is not available in MicroPython
+class CommunicationInterface():
+    def __init__(self):
+        super().__init__()
 
-# setup rtc
-rtc = machine.RTC()
-rtc.ntp_sync("pool.ntp.org")
-utime.sleep_ms(750)
-print('\nRTC Set from NTP to UTC:', rtc.now())
-utime.timezone(7200)
-print('Adjusted from UTC to EST timezone', utime.localtime(), '\n')
+    def setup(self):
+        pass
 
-py = Pytrack()
-l76 = L76GNSS(py, timeout=30)
+    def send(self, data):
+        pass
 
-while (True):
-    coord = l76.coordinates()
-    print("{} - {} - {}".format(coord, rtc.now(), gc.mem_free()))
+
+class WiFiCommunication(CommunicationInterface):
+    def setup(self):
+        print("Setting up WiFi...")
+        print("WiFi set up")
+
+    def send(self, data):
+        print("Sending: " + str(data))
+
+
+class SigFoxCommunication(CommunicationInterface):
+    def setup(self):
+        print("Setting up SigFox...")
+        print("SigFox set up")
+
+    def send(self, data):
+        print("Sending: " + str(data))
+
+
+class CellularCommunication(CommunicationInterface):
+    def setup(self):
+        print("Setting up Cellular...")
+        print("Cellular set up...")
+
+    def send(self, data):
+        print("Sending: " + str(data))
+
+
+class ConsoleCommunication(CommunicationInterface):
+    def setup(self):
+        print("Setting up console...")
+        print("Console set up...")
+
+    def send(self, data):
+        print("Sending: " + str(data))
+        print("{}".format(data))
+
+
+# Unfortunately, the abstract base class (abc) module is not available in MicroPython
+class GNSSInterface():
+    def __init__(self):
+        super().__init__()
+
+    def setup(self):
+        pass
+
+    def getLocation(self):
+        pass
+
+
+class PycomGNSS(GNSSInterface):
+    def setup(self):
+        time.sleep(2)
+        gc.enable()
+
+        # setup rtc
+        rtc = machine.RTC()
+        rtc.ntp_sync("pool.ntp.org")
+        utime.sleep_ms(750)
+        utime.timezone(7200)
+
+        py = Pytrack()
+        self.gnss = L76GNSS(py, timeout=30)
+
+        print("Waiting for GNSS connection...")
+        # Unfortunately, the tuple comparison method - cmp() - is not available in MicroPython
+        while(self.gnss.coordinates()[0] == None and self.gnss.coordinates()[1] == None):
+            pass
+
+        print("GNSS connection acquired")
+
+    def getLocation(self):
+        return self.gnss.coordinates()
+
+
+class CarTracker:
+    def __init__(self, communications, gnss):
+        self.communications = communications
+        self.gnss = gnss
+
+    def beginTracking(self):
+        self.setup()
+        while(True):
+            self.sendLocationData()
+
+    def setup(self):
+        self.gnss.setup()
+        for communication in self.communications:
+            communication.setup()
+
+    def sendLocationData(self):
+        location = self.gnss.getLocation()
+        for communication in self.communications:
+            communication.send(location)
+
+
+communications = [ConsoleCommunication()]
+gnss = PycomGNSS()
+
+carTracker = CarTracker(communications, gnss)
+carTracker.beginTracking()
